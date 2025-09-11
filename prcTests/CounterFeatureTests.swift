81356ea2e2144fd9ba70204529760bf3630b1838
shimizu <noon3m.op.sh@gmail.com>
2025-09-11 21:12:30 +0900

    依存関係の制御

--- prcTests/CounterFeatureTests.swift
+++ prcTests/CounterFeatureTests.swift
@@ -40,4 +40,22 @@ struct CounterFeatureTests {
             $0.isTimerRunning = false
         }
     }
+    
+    // サーバーは毎回異なるfactを返すので動作をテストする方法がない
+    @Test
+    func numberFact() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.numberFact.fetch = { "\($0) is a good number." }
+        }
+        
+        await store.send(.factButtonTapped) {
+            $0.isLoading = true
+        }
+        await store.receive(\.factResponse) {
+            $0.isLoading = false
+            $0.fact = "0 is a good number."
+        }
+    }
 }
